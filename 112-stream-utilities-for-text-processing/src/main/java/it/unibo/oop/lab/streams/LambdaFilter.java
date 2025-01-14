07182c199101a6f5fe267f2917c5944a22618089
Daniel Alejandro <d.alejandrohorna@gmail.com>
2025-01-14 01:45:05 +0100

    im dying

--- 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -7,8 +7,11 @@ import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.io.Serial;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -37,11 +40,28 @@ public final class LambdaFilter extends JFrame {
     @Serial
     private static final long serialVersionUID = 1760990730218643730L;
 
+    private static final String ANY_NON_WORD = "(\\s|\\p{Punct})+";
+
     private enum Command {
         /**
          * Commands.
          */
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+        TO_LOWER("Convert to lowercase", String::toLowerCase),
+        COUNT_CHARS("Character Counter", s -> Integer.toString(s.length())),
+        LINES("Count lines", s -> Long.toString(s.chars().filter(e -> e == '\n').count() + 1)),
+        WORDS("Sort words in alphabetical order", s ->
+            Arrays.stream(s.split(ANY_NON_WORD))
+                .sorted()
+                .collect(Collectors.joining("\n"))),
+        WORDCOUNT("Count words", s ->
+            Arrays.stream(s.split(ANY_NON_WORD))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .map(e -> e.getKey() + " -> " + e.getValue())
+                .collect(Collectors.joining("\n"))
+        );
+        
 
         private final String commandName;
         private final Function<String, String> fun;
@@ -63,34 +83,58 @@ public final class LambdaFilter extends JFrame {
 
     private LambdaFilter() {
         super("Lambda filter GUI");
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
         final JPanel panel1 = new JPanel();
+        
         final LayoutManager layout = new BorderLayout();
+        
         panel1.setLayout(layout);
+        
         final JComboBox<Command> combo = new JComboBox<>(Command.values());
+        
         panel1.add(combo, BorderLayout.NORTH);
+        
         final JPanel centralPanel = new JPanel(new GridLayout(1, 2));
+        
         final JTextArea left = new JTextArea();
+        
         left.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        
         final JTextArea right = new JTextArea();
+        
         right.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        
         right.setEditable(false);
+        
         centralPanel.add(left);
+        
         centralPanel.add(right);
+        
         panel1.add(centralPanel, BorderLayout.CENTER);
+        
         final JButton apply = new JButton("Apply");
+        
         apply.addActionListener(_ ->
             right.setText(
                 ((Command) Objects.requireNonNull(combo.getSelectedItem()))
                     .translate(left.getText())
             )
         );
+        
         panel1.add(apply, BorderLayout.SOUTH);
+        
         setContentPane(panel1);
+        
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        
         final int sw = (int) screen.getWidth();
+        
         final int sh = (int) screen.getHeight();
+        
         setSize(sw / 4, sh / 4);
+        
         setLocationByPlatform(true);
     }
 
